4567fe74a182ba8ea1b5a9aed173b443524eb179
Avenfenix <thedonjuan008p@gmail.com>
2023-10-15 11:09:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ sudo apt-get install -y \
   build-essential \
   pkg-config \
   python3
-npm config set python python3
-npm install --global code-server --unsafe-perm
+sudo npm install --global code-server --unsafe-perm
